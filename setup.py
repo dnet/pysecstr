b11525d03b3fd4ff39c0292f90d9963ab73ee9e4
erik <erik@getvida.io>
2018-08-17 11:49:06 -0400

    py2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='SecureBytes',
-    version='0.2.7',
+    version='0.2.8',
     description='Clears the contents of bytes or integers containing cryptographic material',
     author=u'Erik Aronesty',
     author_email='erik@getvida.io',
