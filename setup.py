c6a2ac19195c06a0a9450ae13fc09f8be29509d6
erik aronesty <erik@q32.com>
2019-05-06 16:43:35 -0400

    include header in ext

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ if sys.platform == "win32":
 
 setup(
     name='SecureBytes',
-    version='0.3.3',
+    version='0.3.4',
     description='Clears the contents of bytes or integers containing cryptographic material',
     author=u'Erik Aronesty',
     long_description=open("README.md").read(),
     author_email='erik@getvida.io',
     url='https://github.com/vidaid/pysecbytes',
     license='MIT',
-    ext_modules=[Extension('SecureBytes', ['pysecbytes.cpp','memstats.c'], extra_compile_args=excomp)],
+    ext_modules=[Extension('SecureBytes', ['pysecbytes.cpp','memstats.c','memstats.h'], extra_compile_args=excomp)],
 )
